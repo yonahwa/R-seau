7b5170a887d88effed23d04c4c495344a54bc08b
yonahwa <2330034@bart.ca>
2024-10-25 16:22:18 -0400

    GET avec le meme manque que HEAD

--- TP1/src/BasedReseau/Serveur.java
+++ TP1/src/BasedReseau/Serveur.java
@@ -9,7 +9,8 @@ import java.io.*;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Serveur {
     private Socket client;
@@ -40,15 +41,16 @@ public class Serveur {
                 //code d'erreur
                 if(!Array[1].startsWith("/") || !Array[2].startsWith("/")){
                     out.writeUTF(Array[2] + "400 Bad Request");
-
+                    continue;
                 }
                 File file = new File("."+ Array[2]);
                 if(!file.exists()){
                     out.writeUTF(Array[2] + "404 Not Found");
-
+                    continue
                 }
                 if(Array[2] != "HTTP/1.1"){
                     out.writeUTF(Array[2] + "505 HTTP Version Not Supported");
+                    continue;
                 }
                 out.writeUTF(Array[2] + "200 Ok");
                 //Date
@@ -75,7 +77,54 @@ public class Serveur {
                 }
 
             }
-            
+            if (Array[0].equals("GET")){
+                out.writeUTF("Get command receive");
+                //code d'erreur
+                if(!Array[1].startsWith("/") || !Array[2].startsWith("/")){
+                    out.writeUTF(Array[2] + "400 Bad Request");
+                    continue;
+                }
+                File file = new File("."+ Array[2]);
+                if(!file.exists()){
+                    out.writeUTF(Array[2] + "404 Not Found");
+                    continue;
+                }
+                if(Array[2] != "HTTP/1.1"){
+                    out.writeUTF(Array[2] + "505 HTTP Version Not Supported");
+                    continue;
+                }
+                out.writeUTF(Array[2] + "200 Ok");
+                //Date
+                Instant currentInstant = Instant.now();
+
+                DateTimeFormatter regulation = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("GMT"));
+
+                String formattedDateTime = regulation.format(currentInstant);
+
+                out.writeUTF("Date et heure en GMT : " + formattedDateTime);
+
+                //Content Type
+                if(Array[1].endsWith(".html")){
+                    out.writeUTF("Content Type : text/html");
+
+                }
+                if(Array[1].endsWith(".js")){
+                    out.writeUTF("Content Type : text/js");
+
+                }
+                if(Array[1].endsWith(".txt")){
+                    out.writeUTF("Content Type : text/txt");
+
+                }
+
+                try {
+                    Files.lines(Paths.get(file.toURI())).forEach(System.out::println);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+
+            }
         }
         System.out.println("Connection Fermer");
 
